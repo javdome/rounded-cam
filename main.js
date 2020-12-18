d6d8390a26681294f87667d805e537589bf21cef
Javi Domenech <javdome@gmail.com>
2020-12-18 07:50:42 +0100

    Little modifications

--- main.js
+++ main.js
@@ -23,6 +23,14 @@ function createWindow () {
 
   win.loadFile('src/index.html');
   win.setAutoHideMenuBar(true);
+
+    // Emitted when the window is closed.
+    win.on('closed', function () {
+      // Dereference the window object, usually you would store windows
+      // in an array if your app supports multi windows, this is the time
+      // when you should delete the corresponding element.
+      mainWindow = null;
+    });
 }
 
 app.whenReady().then(createWindow)
